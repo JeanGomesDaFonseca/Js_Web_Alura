8400dbb4f38833b7cb34f794ec59f5f2ef6def31
JeanGomesDaFonseca <jgomesfonseca21@gmail.com>
2023-08-19 17:03:01 -0300

    refactor code

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const list = document.getElementById("lists");
 const itens = JSON.parse(localStorage.getItem("itens")) || [];
 
 itens.forEach((e) => {
-  console.log(e.name, e.much);
+  createElement(e);
 });
 
 form.addEventListener("submit", (e) => {
@@ -12,29 +12,30 @@ form.addEventListener("submit", (e) => {
   const name = e.target.elements["name"];
   const much = e.target.elements["much"];
 
-  createElement(name.value, much.value);
+  const currentItem = {
+    name: name.value,
+    much: much.value,
+  };
+
+  createElement(currentItem);
+
+  itens.push(currentItem);
+
+  localStorage.setItem("itens", JSON.stringify(itens));
 
   name.value = "";
   much.value = "";
 });
 
-function createElement(name, much) {
+function createElement(item) {
   const newItem = document.createElement("li");
   newItem.classList.add("item");
 
   const numberItem = document.createElement("strong");
-  numberItem.innerHTML = much;
+  numberItem.innerHTML = item.much;
   newItem.appendChild(numberItem);
 
-  newItem.innerHTML += name;
+  newItem.innerHTML += item.name;
 
   list.appendChild(newItem);
-
-  const currentItem = {
-    nome: name,
-    quantidade: much,
-  };
-  itens.push(currentItem);
-
-  localStorage.setItem("itens", JSON.stringify(itens));
 }
